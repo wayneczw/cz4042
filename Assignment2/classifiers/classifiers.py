0a4ffde55485ddd24fa876c835892f44866fd8e1
wayneczw <wayneczw@kokorozashis-MacBook-puro.local>
2018-10-25 13:49:03 +0800

    Part B

--- Assignment2/classifiers/classifiers.py
+++ Assignment2/classifiers/classifiers.py
@@ -1,4 +1,4 @@
-__all__ = ['CNNClassifer']
+__all__ = ['CNNClassifer', 'CNNCharClassifer']
 
 import math
 import tensorflow as tf
@@ -104,6 +104,194 @@ class CNNClassifer():
         self.accuracy = tf.reduce_mean(self.correct_prediction)
 
         self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y_conv))
+        
+        # diff choices of optimizer
+        if self.optimizer == 'GD': self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cross_entropy)
+        elif self.optimizer == 'momentum': self.train_op = tf.train.MomentumOptimizer(self.learning_rate, self.momentum).minimize(self.cross_entropy)
+        elif self.optimizer == 'RMSProp': self.train_op = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.cross_entropy)
+        elif self.optimizer == 'Adam': self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cross_entropy)
+        else: self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cross_entropy)          
+    #end def
+
+
+    def train(self, X_train, Y_train, X_test, Y_test, X_val, Y_val, **kwargs):
+        np.random.seed(10)
+
+        self.train_err = []
+        self.test_acc = []
+        N = len(X_train)
+        idx = np.arange(N)
+        time_to_update = 0
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            self.saver = tf.train.Saver()
+
+            tmp_best_val_err = 1000 # keep track of best_loss
+            _patience = self.patience
+            _epochs = 0
+            for i in range(self.epochs):
+                _epochs += 1
+                np.random.shuffle(idx)
+                X_train = X_train[idx]
+                Y_train = Y_train[idx]
+
+                t = time.time()
+                for _start, _end in zip(range(0, N, self.batch_size), range(self.batch_size, N, self.batch_size)):
+                    if self.drop_out:
+                        self.train_op.run(feed_dict={self.x: X_train[_start:_end], self.y_: Y_train[_start:_end], self._keep_prob: self.keep_prob})
+                    else:
+                        self.train_op.run(feed_dict={self.x: X_train[_start:_end], self.y_: Y_train[_start:_end]})
+                time_to_update += (time.time() - t)
+
+                if self.drop_out:
+                    self.train_err.append(self.cross_entropy.eval(feed_dict={self.x: X_train, self.y_: Y_train, self._keep_prob: self.keep_prob}))
+                    self.test_acc.append(self.accuracy.eval(feed_dict={self.x: X_test, self.y_: Y_test, self._keep_prob: 1.0}))
+                else:
+                    self.train_err.append(self.cross_entropy.eval(feed_dict={self.x: X_train, self.y_: Y_train}))
+                    self.test_acc.append(self.accuracy.eval(feed_dict={self.x: X_test, self.y_: Y_test}))
+                
+                if self.early_stop:
+                    if self.drop_out:
+                        _val_err = self.cross_entropy.eval(feed_dict={self.x: X_val, self.y_: Y_val, self._keep_prob: self.keep_prob})
+                    else: 
+                        _val_err = self.cross_entropy.eval(feed_dict={self.x: X_val, self.y_: Y_val})
+                    if (tmp_best_val_err - _val_err) < self.min_delta:
+                        _patience -= 1
+                        if _patience == 0:
+                            print('Early stopping at {}th iteration'.format(i))
+                            print('-'*50)
+                            break
+                    else:
+                        _patience = self.patience
+                        tmp_best_val_err = _val_err
+                    #end if
+                #end if
+
+                if i % 100 == 0:
+                    print('iter: %d, train error  : %g'%(i, self.train_err[i]))
+                    print('iter: %d, test accuracy  : %g'%(i, self.test_acc[i]))
+                    print('-'*50)
+
+            #end for
+            self.early_stop_epoch = _epochs
+            self.saver.save(sess, ".ckpt/1amodel.ckpt")
+        #end with
+
+        self.time_taken_one_epoch = (time_to_update/_epochs) * 1000
+        return self
+    #end def
+
+
+    def get_feature_maps(self, X):
+        with tf.Session() as sess:
+            self.saver.restore(sess, ".ckpt/1amodel.ckpt")
+            if self.drop_out:
+                c1, p1, c2, p2 = sess.run([self.h_conv1, self.h_pool1, self.h_conv2, self.h_pool2], {self.x: X.reshape(-1, self.input_width*self.input_height*self.num_channels), self._keep_prob: 1.0})
+            else:
+                c1, p1, c2, p2 = sess.run([self.h_conv1, self.h_pool1, self.h_conv2, self.h_pool2], {self.x: X.reshape(-1, self.input_width*self.input_height*self.num_channels)})
+
+        return c1, p1, c2, p2
+#end class
+
+
+class CNNCharClassifer():
+    def __init__(
+        self,
+        input_dim=100, output_dim=10, drop_out=False,
+        keep_prob=0.9, hidden_layer_dict=None,
+        batch_size=128, learning_rate=0.001,
+        l2_beta=0.001, epochs=1000,
+        early_stop=False, patience=20, min_delta=0.001,
+        optimizer='GD', momentum=None,
+        embed=False, vocab_size=256, embed_dim=20,
+        **kwargs
+    ):
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.drop_out = drop_out
+        if self.drop_out:
+            self._keep_prob = tf.placeholder(tf.float32)
+            self.keep_prob = keep_prob
+        self.hidden_layer_dict = hidden_layer_dict
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.l2_beta = l2_beta
+        self.epochs = epochs
+        self.early_stop = early_stop
+        self.patience = patience
+        self.min_delta = min_delta
+        self.optimizer = 'GD'
+        if momentum:
+            self.momentum = momentum
+        self.embed = embed
+        if self.embed:
+            self.vocab_size = vocab_size
+            self.embed_dim = embed_dim
+        self._build_model()
+    #end def
+
+
+    def _build_layer(self, x, cfilters, ckernel_size, cpadding,
+                    pwindow, pstrides, ppadding, **kwargs):
+        #Conv
+        conv = tf.layers.conv2d(x, filters=cfilters,
+                            kernel_size=ckernel_size,
+                            padding=cpadding,
+                            use_bias=True,
+                            kernel_initializer=tf.initializers.truncated_normal(seed=seed),
+                            bias_initializer=tf.zeros_initializer(),
+                            activation=tf.nn.relu)
+        
+        #Pool
+        pool = tf.layers.max_pooling2d(
+                            conv,
+                            pool_size=pwindow,
+                            strides=pstrides,
+                            padding=ppadding)
+
+        return conv, pool
+    #end def
+
+
+    def _build_model(self):
+        self.x = tf.placeholder(tf.int64, [None, self.input_dim])
+        self.y_ = tf.placeholder(tf.int64)
+        self.x_ = tf.reshape(tf.one_hot(self.x, 256), [-1, self.input_dim, 256, 1])
+
+        # embed
+        if self.embed:
+            self.x_embed = tf.contrib.layers.embed_sequence(self.x_, vocab_size=self.vocab_size, embed_dim=self.embed_dim)
+
+        # Conv 1 and pool 1
+        input_dict1 = dict(cfilters=self.hidden_layer_dict['C1']['filters'],
+                            ckernel_size=self.hidden_layer_dict['C1']['kernel_size'],
+                            cpadding=self.hidden_layer_dict['C1']['padding'],
+                            pwindow=self.hidden_layer_dict['S1']['window'],
+                            pstrides=self.hidden_layer_dict['S1']['strides'],
+                            ppadding=self.hidden_layer_dict['S1']['padding'])
+        if self.embed:
+            self.h_conv1, self.h_pool1 = self._build_layer(self.x_embed, **input_dict1)
+        else:
+            self.h_conv1, self.h_pool1 = self._build_layer(self.x_, **input_dict1)
+
+        # Conv 2 and pool 2
+        input_dict2 = dict(cfilters=self.hidden_layer_dict['C2']['filters'],
+                            ckernel_size=self.hidden_layer_dict['C2']['kernel_size'],
+                            cpadding=self.hidden_layer_dict['C2']['padding'],
+                            pwindow=self.hidden_layer_dict['S2']['window'],
+                            pstrides=self.hidden_layer_dict['S2']['strides'],
+                            ppadding=self.hidden_layer_dict['S2']['padding'])
+        self.h_conv2, self.h_pool2 = self._build_layer(self.h_pool1, **input_dict2)
+
+        self.h_pool2 = tf.squeeze(tf.reduce_max(self.h_conv2, 1), squeeze_dims=[1])
+        self.y_conv = tf.layers.dense(self.h_pool2, self.output_dim, activation=tf.nn.relu)
+
+        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(self.y_, self.output_dim), logits=self.y_conv))
+
+        # accuracy
+        self.correct_prediction = tf.cast(tf.equal(tf.argmax(self.y_conv, 1), tf.argmax(tf.one_hot(self.y_, self.output_dim), 1)), tf.float32)
+        self.accuracy = tf.reduce_mean(self.correct_prediction)
         
         # diff choices of optimizer
         if self.optimizer == 'GD': self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cross_entropy)
--- Assignment2/start_project_2b1.py
+++ Assignment2/start_project_2b1.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas
 import tensorflow as tf
 import csv
+from classifiers import CNNCharClassifer
+
 
 MAX_DOCUMENT_LENGTH = 100
 N_FILTERS = 10
@@ -11,7 +13,7 @@ POOLING_STRIDE = 2
 MAX_LABEL = 15
 
 no_epochs = 100
-lr = 0.01
+learning_rate = 0.01
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 seed = 10
@@ -19,91 +21,84 @@ tf.set_random_seed(seed)
 
 def char_cnn_model(x):
   
-  input_layer = tf.reshape(
-      tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])
+    input_layer = tf.reshape(
+        tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])
 
-  with tf.variable_scope('CNN_Layer1'):
-    conv1 = tf.layers.conv2d(
-        input_layer,
-        filters=N_FILTERS,
-        kernel_size=FILTER_SHAPE1,
-        padding='VALID',
-        activation=tf.nn.relu)
-    pool1 = tf.layers.max_pooling2d(
-        conv1,
-        pool_size=POOLING_WINDOW,
-        strides=POOLING_STRIDE,
-        padding='SAME')
+    with tf.variable_scope('CNN_Layer1'):
+        conv1 = tf.layers.conv2d(
+                            input_layer,
+                            filters=N_FILTERS,
+                            kernel_size=FILTER_SHAPE1,
+                            padding='VALID',
+                            activation=tf.nn.relu)
+        pool1 = tf.layers.max_pooling2d(
+                            conv1,
+                            pool_size=POOLING_WINDOW,
+                            strides=POOLING_STRIDE,
+                            padding='SAME')
 
-    pool1 = tf.squeeze(tf.reduce_max(conv1, 1), squeeze_dims=[1])
+        pool1 = tf.squeeze(tf.reduce_max(conv1, 1), squeeze_dims=[1])
 
-  logits = tf.layers.dense(pool1, MAX_LABEL, activation=None)
+        logits = tf.layers.dense(pool1, MAX_LABEL, activation=None)
 
-  return input_layer, logits
+    return input_layer, logits
 
 
 def read_data_chars():
   
-  x_train, y_train, x_test, y_test = [], [], [], []
+    x_train, y_train, x_test, y_test = [], [], [], []
 
-  with open('train_medium.csv', encoding='utf-8') as filex:
-    reader = csv.reader(filex)
-    for row in reader:
-      x_train.append(row[1])
-      y_train.append(int(row[0]))
+    with open('data/train_medium.csv', encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            x_train.append(row[1])
+            y_train.append(int(row[0]))
 
-  with open('test_medium.csv', encoding='utf-8') as filex:
-    reader = csv.reader(filex)
-    for row in reader:
-      x_test.append(row[1])
-      y_test.append(int(row[0]))
-  
-  x_train = pandas.Series(x_train)
-  y_train = pandas.Series(y_train)
-  x_test = pandas.Series(x_test)
-  y_test = pandas.Series(y_test)
-  
-  
-  char_processor = tf.contrib.learn.preprocessing.ByteProcessor(MAX_DOCUMENT_LENGTH)
-  x_train = np.array(list(char_processor.fit_transform(x_train)))
-  x_test = np.array(list(char_processor.transform(x_test)))
-  y_train = y_train.values
-  y_test = y_test.values
-  
-  return x_train, y_train, x_test, y_test
+    with open('data/test_medium.csv', encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            x_test.append(row[1])
+            y_test.append(int(row[0]))
+      
+    x_train = pandas.Series(x_train)
+    y_train = pandas.Series(y_train)
+    x_test = pandas.Series(x_test)
+    y_test = pandas.Series(y_test)
+      
+      
+    char_processor = tf.contrib.learn.preprocessing.ByteProcessor(MAX_DOCUMENT_LENGTH)
+    x_train = np.array(list(char_processor.fit_transform(x_train)))
+    x_test = np.array(list(char_processor.transform(x_test)))
+    y_train = y_train.values
+    y_test = y_test.values
+      
+    return x_train, y_train, x_test, y_test
 
   
 def main():
   
-  x_train, y_train, x_test, y_test = read_data_chars()
+    trainX, trainY, testX, testY = read_data_chars()
+    valX = testX
+    valY = testY
+    trainX = trainX[:100]
+    trainY = trainY[:100]
 
-  print(len(x_train))
-  print(len(x_test))
+    C1_dict = dict(filters=10, kernel_size=[20, 256], padding='VALID')
+    C2_dict = dict(filters=10, kernel_size=[20, 1], padding='VALID')
+    S1_dict = dict(window=4, padding='SAME', strides=2)
+    S2_dict = dict(window=4, padding='SAME', strides=2)
+    hidden_layer_dict = dict(C1=C1_dict, C2=C2_dict, S1=S1_dict, S2=S2_dict)
 
-  # Create the model
-  x = tf.placeholder(tf.int64, [None, MAX_DOCUMENT_LENGTH])
-  y_ = tf.placeholder(tf.int64)
+    init_dict = dict(input_dim=MAX_DOCUMENT_LENGTH, output_dim=MAX_LABEL,
+                hidden_layer_dict=hidden_layer_dict,
+                batch_size=128, learning_rate=learning_rate, epochs=1000,
+                early_stop=True, patience=20, min_delta=0.001, optimizer='Adam')
+    cnn = CNNCharClassifer(**init_dict).train(X_train=trainX, Y_train=trainY,
+                                        X_test=testX, Y_test=testY,
+                                        X_val=valX, Y_val=valY)
+    train_err, test_acc, time_taken_one_epoch, early_stop_epoch = cnn.train_err, cnn.test_acc, cnn.time_taken_one_epoch, cnn.early_stop_epoch
+#end def
 
-  inputs, logits = char_cnn_model(x)
-
-  # Optimizer
-  entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(y_, MAX_LABEL), logits=logits))
-  train_op = tf.train.AdamOptimizer(lr).minimize(entropy)
-
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
-
-  # training
-  loss = []
-  for e in range(no_epochs):
-    _, loss_  = sess.run([train_op, entropy], {x: x_train, y_: y_train})
-    loss.append(loss_)
-
-
-    if e%1 == 0:
-      print('iter: %d, entropy: %g'%(e, loss[e]))
-  
-  sess.close()
 
 if __name__ == '__main__':
   main()
